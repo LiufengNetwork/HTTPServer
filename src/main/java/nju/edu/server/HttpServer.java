075ba5f185a474ade4a07ec1fdc8a55e891ce26e
lujxu <329088775@qq.com>
2017-12-08 01:16:34 +0800

    change

--- src/main/java/nju/edu/server/HttpServer.java
+++ src/main/java/nju/edu/server/HttpServer.java
@@ -3,47 +3,49 @@ package nju.edu.server;
 import nju.edu.HttpStatus;
 import nju.edu.HttpUtils;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * Created by lujxu on 2017/12/7.
  */
-public class HttpServer  implements Runnable {
-    private  Socket socket;
+public class HttpServer implements Runnable {
+    private Socket socket;
+    private Thread serverThread;
 
-    public  HttpServer(Socket socket){
-        this.socket=socket;
+    public HttpServer(Socket socket) {
+        this.socket = socket;
     }
 
-    public static void main(String []args){
-        Socket socket=null;
-        try {
-            // 创建一个监听8000端口的服务器Socket
-            ServerSocket s = new ServerSocket(HttpUtils.port);
-            while (true) {
-                socket = s.accept();
-                System.out.println("connection is published, port=" + socket.getPort());
-                new Thread(new HttpServer(socket)).start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        // 创建一个监听8000端口的服务器Socket
+        ServerSocket s = new ServerSocket(HttpUtils.port);
+        while (true) {
+            socket = s.accept();
+            System.out.println("connection is published, port=" + socket.getPort());
+            HttpServer server=new HttpServer(socket);
+            server.start();
         }
+
     }
 
-    @Override
+    public void start() throws IOException {
+        serverThread = new Thread(this);
+        serverThread.start();
+    }
+
     public void run() {
         try {
-            InputStreamReader inputStreamReader=new InputStreamReader(socket.getInputStream());
-            BufferedReader fromClient=new BufferedReader(inputStreamReader);
-            String requestMessageLine=fromClient.readLine();
-            DataOutputStream outputStream=new DataOutputStream(socket.getOutputStream());
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader fromClient = new BufferedReader(inputStreamReader);
+            String requestMessageLine = fromClient.readLine();
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
             outputStream.write(HttpStatus.OK.getInitialLineBytes());
-            outputStream.writeBytes("\r\n");
+            outputStream.write(HttpUtils.CR);
+            outputStream.write(HttpUtils.LF);
+            System.out.println(requestMessageLine);
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
